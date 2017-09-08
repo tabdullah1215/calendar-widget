f8ffcecb663a73a63d7945fbd388e26b38f9b155
tabdullah1215 <tabdullah1215@live.com>
2017-09-07 23:24:46 -0400

    basic set up

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Calendar extends React.Component {
+    render() {
+        return <h1>I am a Calendar</h1>;
+    }
+};
+
+
+ReactDOM.render(<Calendar />, document.getElementById('root'));
